84dcb0b4e2554d3b113de68043830a0e00a61ef9
katiazanotti <tiuscia.work@gmail.com>
2020-08-12 10:28:53 +0200

    added page

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function ContactPage() {
+  return (
+    <>
+      <h1>Contact us</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
